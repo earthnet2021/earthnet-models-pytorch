9f2c565991fa005b7b18c7c41a11c252f79b0310
Vitus Benson <vbenson@bgc-jena.mpg.de>
2021-04-06 11:40:25 +0200

    Working train

--- scripts/slurmrun.sh
+++ scripts/slurmrun.sh
@@ -13,7 +13,7 @@
 #SBATCH -A p_da_earthnet
 
 # $1 setting.yaml
-# $2 train test both
+# $2 train (test both)
 # $3 test tracks list
 
 arr=($(cat $1 | grep "Setting:"))
@@ -25,26 +25,39 @@ source ~/.bashrc
 echo "Activating Conda Env"
 conda activate pt15
 
-echo "Making Directories"
+cd /home/vibe622c/code/earthnet-models-pytorch/scripts
+
 if [ $1 == "train" || $1 == "both"]; then
     if [ $setting == "en21-std"]; then
-        cd /home/vibe622c/code/codyn-pytorch
-        mkdir -p /tmp/data/release/train/
-        mkdir -p /tmp/data/release/iid_test_split/
+        echo "Making Directories"
+        mkdir -p /tmp/data/train/
         echo "Copying Train"
         cp -rf /scratch/ws/0/vibe622c-codyn/data/release/train/. /tmp/data/release/train/
     elif [ $setting == "en21-veg"]; then
+        echo "Making Directories"
+        mkdir -p /tmp/data/train/
+        mkdir -p /tmp/data/landcover/
+        echo "Copying Train"
+        cp -rf /scratch/ws/0/vibe622c-codyn/data/release/train/. /tmp/data/train/
         echo "Copying Landcover"
-        cp -rf /scratch/ws/0/vibe622c-codyn/data/release/landcover/. /tmp/data/release/landcover/
+        cp -rf /scratch/ws/0/vibe622c-codyn/data/release/landcover/. /tmp/data/landcover/
     else [ $setting == "europe-veg"]; then
         echo "Not Implemented"
+        exit 1
     fi
     
     echo "Start training"
-    srun python train.py configs/resnet18_enc_ndvi.yaml #codyn_lstm_big.yaml # channel_net_hr.yaml #
+    srun python train.py $1
 fi
 
 if [ $1 == "test" || $1 == "both"]; then
-    echo "Copying IID TEst"
-    cp -rf /scratch/ws/0/vibe622c-codyn/data/release/iid_test_split/. /tmp/data/release/iid_test_split/
+    exit 1
+    for track in $4
+    do
+        echo "Copying IID Test"
+        mkdir -p /tmp/data/${track}_test_split/ #Here copy the right track !!!!
+        cp -rf /scratch/ws/0/vibe622c-codyn/data/release/${track}_test_split/. /tmp/data/${track}_test_split/
+        echo "Start testing"
+        srun python test.py $1 $track #Need getting best checkpoint!!!
+    done
 fi
