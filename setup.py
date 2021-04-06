1e2290bfcbb6e79c4180a136608bfd71c8fae81e
Vitus Benson <vbenson@bgc-jena.mpg.de>
2021-04-06 11:42:27 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+install_requires = [
+    ]
+
+scripts = [
+    'scripts/debug.py', 
+    'scripts/train.py', 
+    'scripts/test.py', 
+    'scripts/tune.py', 
+]
+
+setup(name='earthnet-models-pytorch', 
+        version='0.0.1',
+        description="EarthNet Models PyTorch",
+        author="Vitus Benson",
+        author_email="vbenson@bgc-jena.mpg.de",
+        url="https://earthnet.tech",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        classifiers=[
+                "Intended Audience :: Science/Research",
+                "License :: Other/Proprietary License",
+                "Programming Language :: Python :: 3"
+                 ],
+        packages=find_packages(),
+        scripts=scripts,
+        install_requires=install_requires,
+        )
