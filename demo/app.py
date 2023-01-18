80c399c825bd0d12b3fea3d1e8e5dcc4860802a1
Claire <claire.robin@hotmail.fr>
2023-01-18 02:25:14 +0100

    demo finally work

--- demo/app.py
+++ demo/app.py
@@ -1,21 +1,19 @@
 from argparse import ArgumentParser
 import matplotlib.colors as clr
 
-import pytorch_lightning as pl
 import torch
 import os
 from pathlib import Path
-import torchvision
-from PIL import Image
 from earthnet_models_pytorch.model import MODELS
 from earthnet_models_pytorch.task.spatio_temporal import SpatioTemporalTask
 from earthnet_models_pytorch.datamodule import DATASETS
 from earthnet_models_pytorch.metric import NormalizedNashSutcliffeEfficiency
 from earthnet_models_pytorch.utils import parse_setting
-from torch.utils.data import Dataset, DataLoader
 
 
 import streamlit as st
+
+st.set_page_config(layout="wide")
 
 
 cmap_veg = clr.LinearSegmentedColormap.from_list(
@@ -93,47 +91,63 @@ def metric(prediction, minicube):
     score = metric.compute_sample(prediction, minicube)
     return score
 
-
-def visualisation(targs, preds, score):
+def visualisation_targ(targs):
     sentinel = targs["dynamic"][0]
-    lc = targs["landcover"]
-    nrow = 9 if sentinel.shape[1] % 9 == 0 else 10
-    """rgb = torch.cat(
+    ndvi = sentinel[0, -10:, 0, ...]
+    rgb = torch.cat(
         [
-            sentinel[0, -preds.shape[1], 2, ...].unsqueeze(1) * 10000,
-            sentinel[0, -preds.shape[1], 1, ...].unsqueeze(1) * 10000,
-            sentinel[0, -preds.shape[1], 0, ...].unsqueeze(1) * 10000,
+            sentinel[0, -10:, 3, ...].unsqueeze(3) * 10,
+            sentinel[0, -10:, 2, ...].unsqueeze(3) * 10,
+            sentinel[0, -10:, 1, ...].unsqueeze(3) * 10,
         ],
-        dim=1,
-    )"""
-    grid = preds.squeeze(2).squeeze(0).cpu().detach().numpy()
-    # grid = cmap_veg(grid)
-    st.image(grid[1, ...])
-    # cols = st.columns(grid.shape[0])
-    # for col_num in range(grid.shape[0]):
-    # img = Image.fromarray(grid[col_num, ...])
-    # print(grid[col_num, ...].shape)
-    # cols[col_num].image(grid[col_num, ...])
+        dim=3,
+    ).cpu().detach().numpy()
+    print(rgb)
+    grid = ndvi.squeeze(2).squeeze(0).cpu().detach().numpy()
+    grid = cmap_veg(grid)
+    
+    st.write("Satellite data")
+    cols = st.columns(grid.shape[0])
+    for col_num in range(grid.shape[0]):
+        cols[col_num].image(rgb[col_num, ...], clamp=True, channels='RGB', use_column_width=True)
 
+    st.write("Target")
+    cols = st.columns(grid.shape[0])
+    for col_num in range(grid.shape[0]):
+        cols[col_num].image(grid[col_num, ...], use_column_width=True)
+
+    return
+
+def visualisation_pred(preds, score):
+    grid = preds.squeeze(2).squeeze(0).cpu().detach().numpy()
+    grid = cmap_veg(grid)
+
+    cols = st.columns(grid.shape[0])
+    for col_num in range(grid.shape[0]):
+        cols[col_num].image(grid[col_num, ...], use_column_width=True)
     return
 
 
 if __name__ == "__main__":
-    setting = "experiments/en23/context-convlstm/baseline_RGBNR/full_train/setting.yaml"
-    checkpoint = "experiments/en23/context-convlstm/baseline_RGBNR/full_train/checkpoints/last.ckpt"
+    setting = os.getcwd() + "/demo/setting.yaml"
+    checkpoint = os.getcwd() + "/demo/last.ckpt"
     track = "iid"
 
     setting_dict = parse_setting(setting, track)
 
-    st.title("Demo of a model for vegetation greeness on the Earthnet dataset")
+    st.title("Forecast vegetation greeness on the Earthnet2023 dataset")
     minicube = load_minicube()
     model = load_model(checkpoint)
 
-    result = 1  # st.button("Run on minicube")
-    if result:
-        st.write("Calculating prediction...")
-        with torch.no_grad():
-            prediction, _ = model(minicube, 1)
-        # score = metric(prediction, minicube)
-        score = 1
-        visualisation(minicube, prediction, score)
+    result = st.button("Run on minicube")
+    if result:    
+        visualisation_targ(minicube)
+        with st.empty():
+            st.write("Calculating prediction...")
+            with torch.no_grad():
+                prediction, _ = model(minicube, 1)
+            # score = metric(prediction, minicube)
+
+            score = 1
+            st.write("Prediction")
+        visualisation_pred(prediction, score)
