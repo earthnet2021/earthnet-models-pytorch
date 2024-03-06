7002fe356d586c9f4a674cf142f43ad96d9af1df
vitusbenson <vitus.benson@icloud.com>
2024-03-06 21:54:58 +0100

    v0.1.0 for CVPR camera ready release

--- earthnet_models_pytorch/__init__.py
+++ earthnet_models_pytorch/__init__.py
@@ -1,9 +1,9 @@
 """EarthNet Models PyTorch
 A library with models for Earth surface forecasting.
 """
-__version__ = "0.0.1"
-__author__ = 'Vitus Benson'
-__credits__ = 'Max-Planck-Institute for Biogeochemistry'
+__version__ = "0.1.0"
+__author__ = 'Vitus Benson, Claire Robin'
+__credits__ = 'Max Planck Institute for Biogeochemistry'
 
 from earthnet_models_pytorch.utils import str2bool
 import earthnet_models_pytorch.data
