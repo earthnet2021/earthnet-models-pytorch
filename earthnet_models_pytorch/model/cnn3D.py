848c2be9a92656f8caab644ae785c5f341f90664
Prasoon Pandey <pandey.prasoon2000@gmail.com>
2024-04-10 10:29:29 +0200

    added 3d cnn file

--- earthnet_models_pytorch/model/cnn3D.py
+++ earthnet_models_pytorch/model/cnn3D.py
@@ -5,6 +5,7 @@ import logging
 import ast
 import torch.nn as nn
 import torch
+torch.autograd.set_detect_anomaly(True)
 from earthnet_models_pytorch.utils import str2bool
 
 logging.basicConfig(filename='forward.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -15,7 +16,6 @@ class StackedConv3D(nn.Module):
 
         self.hparams = hparams
         kernel_size = self.hparams.kernel_size
-        final_kernel_size = 23
         
         # Determine if NDVI prediction is required based on the specified settings
         self.ndvi_pred = (hparams.setting in ["en21-veg", "europe-veg", "en21x", "en22", "en23"])
@@ -35,23 +35,23 @@ class StackedConv3D(nn.Module):
         )
         
         self.conv2 = nn.Sequential(
-            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=kernel_size, stride=self.hparams.stride_size, padding = "same", bias=self.hparams.bias),
-            nn.BatchNorm3d(64),
+            nn.Conv3d(in_channels=64, out_channels=49, kernel_size=kernel_size, stride=self.hparams.stride_size, padding = "same", bias=self.hparams.bias),
+            nn.BatchNorm3d(49),
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=kernel_size, stride=self.hparams.stride_size, padding = "same", bias=self.hparams.bias),
+            nn.Conv3d(in_channels=49, out_channels=64, kernel_size=kernel_size, stride=self.hparams.stride_size, padding = "same", bias=self.hparams.bias),
             nn.BatchNorm3d(64),
         )
 
         self.conv4 = nn.Sequential(
-            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=kernel_size, stride=self.hparams.stride_size, padding = "same", bias=self.hparams.bias),
-            nn.BatchNorm3d(64),
+            nn.Conv3d(in_channels=64, out_channels=49, kernel_size=kernel_size, stride=self.hparams.stride_size, padding = "same", bias=self.hparams.bias),
+            nn.BatchNorm3d(49),
         )
 
         # Define the final convolutional layer and activation function
         self.final_conv = nn.Sequential(
-            nn.Conv3d(in_channels=64, out_channels=1, kernel_size=(5, kernel_size, kernel_size), stride=(kernel_size, 1, 1), padding=(0, 1, 1), bias=self.hparams.bias),
+            nn.Conv3d(in_channels=49, out_channels=1, kernel_size=(5, 3, 3), stride=(3, 1, 1), padding=(0, 1, 1), bias=self.hparams.bias),
         )
 
         # Final activation function
@@ -80,7 +80,7 @@ class StackedConv3D(nn.Module):
         parser.add_argument("--input", type = str, default = "RGBNR")
         parser.add_argument("--target", type = str, default = False)
         parser.add_argument("--single_frame_prediciton", type = str2bool, default=True)
-        parser.add_argument("--skip_connections", type = str2bool, default = False)
+        parser.add_argument("--residual_connections", type = str2bool, default = False)
         parser.add_argument("--teacher_forcing", type = str2bool, default = False)
         parser.add_argument("--use_weather", type = str2bool, default = True)
         parser.add_argument("--spatial_shuffle", type = str2bool, default = False)
@@ -110,9 +110,6 @@ class StackedConv3D(nn.Module):
 
         # sentinel 2 bands
         sentinel = data["dynamic"][0][:, : context_length, ...]
-        # logging.debug(f"Sentinel size: {sentinel.size()}")
-        # logging.debug(f"Sentinel: {sentinel}")
-
 
         # Extract the target for the teacher forcing method
         if self.hparams.teacher_forcing and self.training:
@@ -121,7 +118,6 @@ class StackedConv3D(nn.Module):
             ]
 
         weather = data["dynamic"][1].unsqueeze(3).unsqueeze(4)
-        # logging.debug(f"Weather size: {weather.size()}")
 
         static = data["static"][0]
 
@@ -144,7 +140,6 @@ class StackedConv3D(nn.Module):
 
         # Expand static tensor along the time dimension
         stacked_static = static.unsqueeze(1).expand(-1, t_sentinel + self.hparams.target_length, -1, -1, -1) # (b, 30, c_s, h, w)
-        # logging.debug(f"stacked_static size: {stacked_static.size()}")
 
         # Concatenate sentinel and expanded_static along the channel dimension
         stacked_x = torch.cat((sentinel, stacked_static[:, :context_length, ...]), dim=2) # (b, 23, c+c_s, h, w)
@@ -160,81 +155,47 @@ class StackedConv3D(nn.Module):
                     .repeat(1, 1, 128, 128)
                 )
                 weather_array.append(weather_t.squeeze(1))
-                # logging.debug(f"stacked_x_weather size: {stacked_x_weather.size()}")
             
             stacked_weather = torch.stack(weather_array, dim = 1).contiguous() # (b, 30, c_w, h, w)
 
             # Concatenate stacked_x and stacked_weather along the channel dimension
             stacked_x = torch.cat((stacked_x, stacked_weather[:, :context_length, ...]), dim=2).contiguous() # (b, 23, c+c_s+c_w, h, w)
-            # logging.debug(f"stacked_x after weather size: {stacked_x.size()}")  
 
-        # Permute the dimensions to match the cnn3d format
+        # Permute the dimensions to match cnn3d input format
         stacked_x = stacked_x.permute(0, 2, 1, 3, 4).contiguous() # (b, c+c_s+c_w, 30, h, w)
-        # logging.debug(f"stacked_x size after weather for cnn3d: {stacked_x.size()}")
 
         if self.hparams.single_frame_prediciton:
-            target_length= self.hparams.target_length
+            num = self.hparams.target_length # total loops (TODO)
         else:
-            target_length = 1
+            num = 1
 
         output = []
 
-        # Forecasting network
-        for t in range(target_length):
+        x = self.conv1(stacked_x)
+        x = nn.ReLU()(x)
+        x = self.conv2(x)
 
-            # Teacher forcing scheduled sampling
-            # if (
-            #     self.hparams.teacher_forcing
-            #     and self.training
-            #     and torch.bernoulli(teacher_forcing_decay)
-            # ):
-            #     x = target[:, t, 0, ...].unsqueeze(1)
+        # Add residual connection
+        if self.hparams.residual_connections:
+            x = x + stacked_x
 
-            # if (t==0):
-            #     stacked_x_input = stacked_x[:, :, t : t + context_length, ...]
-            # else:
-            #     static_frame = stacked_static[:, context_length + t - 1, ...].unsqueeze(1).permute(0, 2, 1, 3, 4) # (b, 8, 1, h, w)             
-            #     next_frame_with_static = torch.cat((x, static_frame), dim=1) # (b, 8+1, 1, h, w)
+        x = nn.ReLU()(x)
+        x = self.conv3(x)
+        x = nn.ReLU()(x)
+        x = self.conv4(x)
 
-            #     if self.hparams.use_weather:
-            #         last_frame_weather = stacked_weather[:, context_length + t - 1, ...].unsqueeze(1).permute(0, 2, 1, 3, 4) # (b, 40, 1, h, w)
-            #         next_frame_combined = torch.cat((next_frame_with_static, last_frame_weather), dim=1) # (b, 8+1+40, 1, h, w)
+        # Add residual connection
+        if self.hparams.residual_connections:
+            x = x + stacked_x
 
-            #     # logging.debug(f"next_frame_combined size: {next_frame_combined.size()}")
-                
-            #     # remove last frame
-            #     stacked_x_input = stacked_x_input[:, :, 1:, ...] # (b, c+8+40, context_length-1, h, w)
+        x = nn.ReLU()(x)
+    
+        x = self.final_conv(x)        
+        # Output
+        x = self.activation_output(x)
 
-            #     # Append next_frame_combined to combined
-            #     stacked_x_input = torch.cat((stacked_x_input, next_frame_combined), dim=2).contiguous()
-
-            stacked_x_input = stacked_x
-
-            x = self.conv1(stacked_x_input)
-            x = nn.ReLU()(x)
-            x = self.conv2(x)
-            x = nn.ReLU()(x)
-            x = self.conv3(x)
-            x = nn.ReLU()(x)
-            x = self.conv4(x)
-            x = nn.ReLU()(x)
-            
-            # logging.debug(f"Pre-output size: {x.size()}")
-
-            x = self.final_conv(x)
-            # logging.debug(f"Output size: {x.size()}")
-
-            # Add the previous prediction for skip connections
-            # if self.hparams.skip_connections:
-            #     pred = pred + pred_previous
-
-            # Residual connections
-            # x += stacked_x_input
-            # Output
-            x = self.activation_output(x)
-
-            # nf = x.squeeze(2)
-            # output += [nf.unsqueeze(1)]
+        # nf = x.squeeze(2)
+        # output += [nf.unsqueeze(1)]
 
         if self.hparams.single_frame_prediciton:
             preds = torch.cat(output, dim=1) 
@@ -242,6 +203,6 @@ class StackedConv3D(nn.Module):
             preds = x.permute(0, 2, 1, 3, 4).contiguous()
                 
         # preds = torch.stack(output, dim = 1).contiguous()
-        # logging.debug(f"Output size: {preds.size()}")
+        logging.debug(f"Output size: {preds.size()}")
 
         return preds, {}
